a57b2d5a5554b0aa316e69354fcbb1b9e81d58b2
GautamR123 <75315987+GautamR123@users.noreply.github.com>
2021-02-02 18:59:00 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var ball;
+var database,position;
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+var location_of_ball=database.ref("ball/position")
+location_of_ball.on("value", readOperation,showError)
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}}
+
+function writePosition(x,y){
+    //ball.x = ball.x + x;
+    //ball.y = ball.y + y;
+    database.ref("ball/position").set({
+        x: ball.x + x,
+        y: ball.y + y
+    })
+}
+function readOperation(data){
+position=data.val()
+ball.x=position.x
+ball.y=position.y
+}
+function showError(){
+    console.log("This is the error")
+}
